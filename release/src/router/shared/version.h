28ecdea94b44fcabb3dd64dacc2471d6fbf85197
Eric Sauvageau <rmerl@lostrealm.ca>
2017-09-04 23:03:46 -0400

    Add missing gitignore files; force some empty directories to git, needed by the build process

---
+++ release/src/router/shared/version.h
@@ -0,0 +1,10 @@
+#ifndef RTVERSION
+#define RT_MAJOR_VERSION "3.0"
+#define RT_MINOR_VERSION "0.4"
+#define RT_VERSION "3.0.0.4"
+#define RT_SERIALNO "380.57"
+#define RT_EXTENDNO "alpha3-g58f4a86"
+#define RT_SWPJVERNO ""
+#define RT_BUILD_NAME "RT-N66U"
+#define RT_BUILD_INFO "Fri Dec  4 15:08:38 UTC 2015 merlin@58f4a86"
+#endif

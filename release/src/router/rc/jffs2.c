7b3ac806a6e5f9cb4217a96f8cb3d265df25ec2f
Eric Sauvageau <rmerl@lostrealm.ca>
2017-09-15 00:10:24 -0400

    rc: make initial jffs2 SSL folder permissions more secure

--- release/src/router/rc/jffs2.c
+++ release/src/router/rc/jffs2.c
@@ -340,7 +340,7 @@ void start_jffs2(void)
 
 	if (!check_if_dir_exist("/jffs/scripts/")) mkdir("/jffs/scripts/", 0755);
 	if (!check_if_dir_exist("/jffs/configs/")) mkdir("/jffs/configs/", 0755);
-	if (!check_if_dir_exist("/jffs/ssl/")) mkdir("/jffs/ssl/", 0755);
+	if (!check_if_dir_exist(UPLOAD_CERT_FOLDER)) mkdir(UPLOAD_CERT_FOLDER, 0600);
 }
 
 void stop_jffs2(int stop)

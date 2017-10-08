8e2126ab27c937bb2b9acbc53d1f4372a16dc236
Eric Sauvageau <rmerl@lostrealm.ca>
2017-10-08 03:02:21 -0400

    webui: move traffic monitoring-related functions from merlin.js to tmhist.js

--- release/src/router/www/merlin.js
+++ release/src/router/www/merlin.js
@@ -14,25 +14,6 @@ function setRadioValue(obj,val) {
 	}
 }
 
-function cmpFloat(a, b) {
-        a = parseFloat(a);
-        b = parseFloat(b);
-        return ((isNaN(a)) ? -Number.MAX_VALUE : a) - ((isNaN(b)) ? -Number.MAX_VALUE : b);
-}
-
-function cmpDualFields(a, b) {
-	if (cmpHist(a, b) == 0)
-		return aton(a[1])-aton(b[1]);
-	else
-		return cmpHist(a,b);
-}
-
-function getYMD(n) {
-	// [y,m,d]
-	return [(((n >> 16) & 0xFF) + 1900), ((n >>> 8) & 0xFF), (n & 0xFF)];
-}
-
-
 /**
 *
 *  Base64 encode / decode

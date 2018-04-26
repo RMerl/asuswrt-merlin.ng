35c39a58f99c6eb66446bd8bfb38d2c117f9422c
Eric Sauvageau <rmerl@lostrealm.ca>
2018-04-26 01:22:08 -0400

    rom: Increase timeout for gettunnelip's ministun call; set VPN IP to "unknown" if connection fails
    This is to prevent routers from constantly flooding the listed stun servers with
    connection attempts in case of failure.  The "Refresh" button on the VPN Client
    page now allows to force a new lookup (stopping/starting VPN client will also
    accomplish the same thing).

--- release/src/router/rom/apps_scripts/gettunnelip.sh
+++ release/src/router/rom/apps_scripts/gettunnelip.sh
@@ -4,9 +4,9 @@ INSTANCE=$1
 servers="stun.l.google.com:19302 stun.stunprotocol.org"
 
 for server in $servers; do
-	result=$(/usr/sbin/ministun -t 1000 -c 1 -i tun1$INSTANCE $server 2>/dev/null)
+	result=$(/usr/sbin/ministun -t 5000 -c 1 -i tun1$INSTANCE $server 2>/dev/null)
 	[ $? -eq 0 ] && break
-	result=
+	result="unknown"
 done
 
 nvram set "vpn_client${INSTANCE}_rip"=$result

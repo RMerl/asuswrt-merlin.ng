e71a06149d1f2efbf559a4ace5d601c830c0eef6
Eric Sauvageau <rmerl@lostrealm.ca>
2018-06-23 01:48:20 -0400

    Remove APP-IPK source files (unused)

--- release/src/router/APP-IPK/AiCloud-ipk/opt/etc/aicloud_UI/js/davclient_tools.js
+++
@@ -1,7 +0,0 @@
-var DAVTOOLS_PATH = '/smb/js/davclient.js/';
-
-document.write('<script type="text/javascript" src="'+DAVTOOLS_PATH+'jsbase/exception.js"></script>');
-document.write('<script type="text/javascript" src="'+DAVTOOLS_PATH+'jsbase/string.js"></script>');
-document.write('<script type="text/javascript" src="'+DAVTOOLS_PATH+'minisax.js/minisax.min.js"></script>');
-document.write('<script type="text/javascript" src="'+DAVTOOLS_PATH+'dommer/dommer.min.js"></script>');
-document.write('<script type="text/javascript" src="'+DAVTOOLS_PATH+'davclient.js"></script>');
--- release/src/router/APP-IPK/AiCloud-ipk/opt/etc/apps_asus_script/aicloud_nvram_change.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-touch /tmp/aicloud_nvram.txt
-echo "$1 $2"|sed -e 's/\//\\&/g'>/tmp/aicloud_nvram.txt

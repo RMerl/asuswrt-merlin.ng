b49d9283f0700a885a52fd1b9eab2d607f5635c8
Eric Sauvageau <rmerl@lostrealm.ca>
2018-06-23 23:37:21 -0400

    dnsmasq: log packet resize reports at debug level instead of warning since they are too frequent

--- release/src/router/dnsmasq/src/forward.c
+++ release/src/router/dnsmasq/src/forward.c
@@ -953,7 +953,7 @@ void reply_query(int fd, int family, time_t now)
       server->edns_pktsz = SAFE_PKTSZ;
       server->pktsz_reduced = now;
       prettyprint_addr(&server->addr, daemon->addrbuff);
-      my_syslog(LOG_WARNING, _("reducing DNS packet size for nameserver %s to %d"), daemon->addrbuff, SAFE_PKTSZ);
+      my_syslog(LOG_DEBUG, _("reducing DNS packet size for nameserver %s to %d"), daemon->addrbuff, SAFE_PKTSZ);
     }
 
     

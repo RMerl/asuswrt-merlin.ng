92d742117e830d98b0f6521e38eace04e6ebeaa3
Eric Sauvageau <rmerl@lostrealm.ca>
2018-06-24 00:07:45 -0400

    dnsmasq: rc: in addition to private IP reverse lookups, also filter out non-FQDN hostname forward queries from being sent to upstream servers, unless told otherwise

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -1361,9 +1361,11 @@ void start_dnsmasq(void)
 	if (*value) {
 		fprintf(fp, "domain=%s\n"
 			    "expand-hosts\n", value);	// expand hostnames in hosts file
-		if (nvram_get_int("lan_dns_fwd_local") != 1)
-			fprintf(fp, "bogus-priv\n"
-			            "local=/%s/\n", value);	// Don't forward local queries upstream
+	}
+	if (nvram_get_int("lan_dns_fwd_local") != 1) {
+		fprintf(fp, "bogus-priv\n"
+		            "domain-needed\n"
+		            "local=/%s/\n", value);	// Don't forward local queries upstream
 	}
 
 	if ((is_routing_enabled() && nvram_get_int("dhcp_enable_x"))

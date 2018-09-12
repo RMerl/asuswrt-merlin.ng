498ffa354497e95310766949e321c2d847b24c51
Benno Schulenberg <bensberg@telfort.nl>
2018-09-12 17:29:38 -0400

    nano: bindings: when Ctrl+Shift+Delete has no keycode, don't use KEY_BSP (backport from upstream)
    When curses gives no code for Ctrl+Shift+Delete, do not fall back
    to KEY_BACKSPACE, because then ^H and/or <Backspace> get bound to
    'cutwordleft'.
    
    This fixes https://savannah.gnu.org/bugs/?54642.
    
    Bug was introduced with version 3.0, commit e6429e78.

--- release/src/router/nano/src/nano.c
+++ release/src/router/nano/src/nano.c
@@ -2580,7 +2580,7 @@ int main(int argc, char **argv)
 	controlhome = get_keycode("kHOM5", CONTROL_HOME);
 	controlend = get_keycode("kEND5", CONTROL_END);
 	controldelete = get_keycode("kDC5", CONTROL_DELETE);
-	controlshiftdelete = get_keycode("kDC6", KEY_BACKSPACE);
+	controlshiftdelete = get_keycode("kDC6", CONTROL_SHIFT_DELETE);
 #ifndef NANO_TINY
 	/* Ask for the codes for Shift+Control+Left/Right/Up/Down. */
 	shiftcontrolleft = get_keycode("kLFT6", SHIFT_CONTROL_LEFT);

c5926972790c97788a47fde99f7545f6fb5e0915
Eric Sauvageau <rmerl@lostrealm.ca>
2018-10-31 01:28:14 -0400

    httpd: prevent resource leak in parse_openvpn_status() if one of the fopen() calls fails

--- release/src/router/httpd/openvpn_options.c
+++ release/src/router/httpd/openvpn_options.c
@@ -736,8 +736,7 @@ void parse_openvpn_status(int unit){
 					fprintf(fpo, "NoUsername\n");
 			}
 		}
-		if(fpi) fclose(fpi);
-		if(fpo) fclose(fpo);
 	}
+	if(fpi) fclose(fpi);
+	if(fpo) fclose(fpo);
 }
-

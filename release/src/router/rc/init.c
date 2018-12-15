3d9b75b612d71e23b0e71fed05fc5f75459b7cbb
John Bacho <john.9527@hotmail.com>
2018-12-15 10:10:36 -0700

    init: stop logger after usb shutdown during reboot

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -10516,7 +10516,6 @@ int init_main(int argc, char *argv[])
 
 			stop_lan();
 			stop_vlan();
-			stop_logger();
 
 #if defined(RTCONFIG_ALPINE) || defined(RTCONFIG_LANTIQ)
 			_dprintf("sync during rebooting...\n");
@@ -10540,6 +10539,7 @@ int init_main(int argc, char *argv[])
 #endif
 				}
 #endif
+				stop_logger();
 #if defined(RTCONFIG_QCA) && defined(RTCONFIG_QCA8033)
 				stop_lan_port();
 				start_lan_port(5);
@@ -10555,6 +10555,7 @@ int init_main(int argc, char *argv[])
 			// SIGHUP (RESTART) falls through
 
 		case SIGUSR2:		/* START */
+			stop_logger();
 			start_logger();
 #if defined(RTCONFIG_QCA)
 			logmessage("INIT", "firmware version: %s_%s_%s\n", nvram_safe_get("firmver"), nvram_safe_get("buildno"), nvram_safe_get("extendno"));

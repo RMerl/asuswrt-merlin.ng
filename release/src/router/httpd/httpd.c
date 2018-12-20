3460477cdf279d815735c143eb8278e158f52410
Vladislav Grishenko <themiron@mail.ru>
2018-12-21 02:00:34 +0500

    httpd: fix crashes on hnd

--- release/src/router/httpd/httpd.c
+++ release/src/router/httpd/httpd.c
@@ -1917,12 +1917,6 @@ search_desc (pkw_t pkw, char *name)
 #endif
 #endif //TRANSLATE_ON_FLY
 
-void reapchild()	// 0527 add
-{
-	signal(SIGCHLD, reapchild);
-	wait(NULL);
-}
-
 int main(int argc, char **argv)
 {
 	usockaddr usa;
@@ -1987,8 +1981,7 @@ int main(int argc, char **argv)
 
 	/* Ignore broken pipes */
 	signal(SIGPIPE, SIG_IGN);
-	signal(SIGCHLD, reapchild);	// 0527 add
-	signal(SIGUSR1, update_wlan_log);
+	signal(SIGCHLD, chld_reap);
 
 #ifdef RTCONFIG_HTTPS
 	//if (do_ssl)

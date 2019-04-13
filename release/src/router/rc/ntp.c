10f11f852c164574b14308e5aaddcdcbc23148f1
Vladislav Grishenko <themiron@mail.ru>
2019-04-13 18:14:14 +0500

    rc: fix comment placement, cosmetics

--- release/src/router/rc/ntp.c
+++ release/src/router/rc/ntp.c
@@ -75,11 +75,11 @@ static void ntp_service()
 			notify_rc("restart_stubby");
 #endif
 #ifdef RTCONFIG_DNSSEC
+		/* Should be safe w/o DNSSEC configured */
 		if (nvram_get_int("dnssec_enable"))
 			kill_pidfile_s("/var/run/dnsmasq.pid", SIGINT);
 #endif
 #ifdef RTCONFIG_DISK_MONITOR
-		/* Should be safe w/o DNSSEC configured */
 		notify_rc("restart_diskmon");
 #endif
 	}

ae58784797351d80f1c312da7b93e9ca7a32edd6
Eric Sauvageau <rmerl@lostrealm.ca>
2019-04-18 00:08:36 -0400

    rc: add postconf/custom config support for stubby

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -1953,7 +1953,10 @@ void start_stubby(void)
 	if (nv)
 		free(nv);
 
+	append_custom_config("stubby", fp);
 	fclose(fp);
+	use_custom_config("stubby.yml", stubby_config);
+	run_postconf("stubby", stubby_config);
 	chmod(stubby_config, 0644);
 
 	if (nvram_get_int("stubby_debug")) {

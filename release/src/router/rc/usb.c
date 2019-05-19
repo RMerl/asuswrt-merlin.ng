fb832254b93d055b894c2c8c2ae773ace93cdfed
Eric Sauvageau <rmerl@lostrealm.ca>
2019-05-19 13:28:18 -0400

    rc: fix parameter order when launching wsdd2

--- release/src/router/rc/usb.c
+++ release/src/router/rc/usb.c
@@ -5586,9 +5586,9 @@ void start_wsdd()
 	char bootparms[64];
 	char *wsdd_argv[] = { "/usr/sbin/wsdd2",
 				"-d",
-				"-b",
 				"-i",
 				nvram_safe_get("lan_ifname"),
+				"-b",
 				NULL,	// boot parameters
 				NULL };
 	stop_wsdd();

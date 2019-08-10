78855e6ad2583b7fc96e26aed8f23297a380e7eb
Eric Sauvageau <merlin@asuswrt-merlin.net>
2019-08-10 15:36:46 -0400

    rc: revert start_lan_port() backport from upstream which broke AP mode on the RT-AC87U.
    Revert "rc: backport start_lan_port() fix for RT-AC87U from GPL 382-51640"
    
    This reverts commit 7d6b8a20530c3c411f3197409fa53335fa456cb8.

--- release/src/router/rc/lan.c
+++ release/src/router/rc/lan.c
@@ -6250,7 +6250,7 @@ void start_lan_port(int dt)
 	}
 
 #ifdef RTCONFIG_QTN
-	if (nvram_get_int("qtn_ready") == 0)
+	if (nvram_get_int("qtn_ready") == 1)
 		dbG("do not start lan port due to QTN not ready\n");
 	else
 		lanport_ctrl(1);

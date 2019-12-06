ef8cc18200000794834bee5c6dca53897f095ef2
Eric Sauvageau <merlin@asuswrt-merlin.net>
2019-12-05 22:52:51 -0500

    inadyn: re-disable cert validation for AsusDDNS - their server is once again using an expired certificate

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -3517,6 +3517,7 @@ start_ddns(void)
 		service = "update@asus.com";
 		user = get_lan_hwaddr();
 		passwd = nvram_safe_get("secret_code");
+		asus_ddns = 1;
 	}
 	else if (strcmp(server, "DOMAINS.GOOGLE.COM") == 0)
 		service = "default@domains.google.com";
@@ -3593,6 +3594,10 @@ start_ddns(void)
 				fprintf(fp, "wildcard = true\n");
 
 			fprintf(fp, "}\n");
+#if 1
+			if (asus_ddns == 1)
+				fprintf(fp, "secure-ssl = false\n");
+#endif
 
 			append_custom_config("inadyn.conf", fp);
 
@@ -3760,6 +3765,9 @@ asusddns_reg_domain(int reg)
 		fprintf(fp, "username = %s\n", get_lan_hwaddr());
 		fprintf(fp, "password = %s\n", nvram_safe_get("secret_code"));
 		fprintf(fp, "}\n");
+#if 1
+		fprintf(fp, "secure-ssl = false\n");
+#endif
 		fclose(fp);
 
 		if((time_fp=fopen("/tmp/ddns.cache","w"))) {
@@ -3835,6 +3843,9 @@ _dprintf("%s: do inadyn to unregister! unit = %d wan_ifname = %s nserver = %s ho
 		fprintf(fp, "username = %s\n", get_lan_hwaddr());
 		fprintf(fp, "password = %s\n", nvram_safe_get("secret_code"));
 		fprintf(fp, "}\n");
+#if 1
+		fprintf(fp, "secure-ssl = false\n");
+#endif
 		fclose(fp);
 
 /*

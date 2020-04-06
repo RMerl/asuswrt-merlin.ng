e8a4bdfe4827547a1e3e29a923de0be90d081e86
Vladislav Grishenko <themiron@mail.ru>
2020-04-06 23:28:09 +0500

    dropbear: Add Chacha20-Poly1305 authenticated encryption
    https://github.com/themiron/dropbear/commit/9836d72c2c62ba149dcd9296543ed73f384ca6b2

--- release/src/router/dropbear/session.h
+++ release/src/router/dropbear/session.h
@@ -38,6 +38,7 @@
 #include "chansession.h"
 #include "dbutil.h"
 #include "netio.h"
+#include "chachapoly.h"
 
 void common_session_init(int sock_in, int sock_out);
 void session_loop(void(*loophandler)(void)) ATTRIB_NORETURN;
@@ -77,6 +78,9 @@ struct key_context_directional {
 		symmetric_CBC cbc;
 #if DROPBEAR_ENABLE_CTR_MODE
 		symmetric_CTR ctr;
+#endif
+#if DROPBEAR_CHACHA20POLY1305
+		dropbear_chachapoly_state chachapoly;
 #endif
 	} cipher_state;
 	unsigned char mackey[MAX_MAC_LEN];

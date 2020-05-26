5516729e499cf3f81ead6a57ffe9938cdef7f80f
Eric Sauvageau <merlin@asuswrt-merlin.net>
2020-05-25 23:29:56 -0400

    httpd: rework gencert.sh script responsible for generating web certificate
     - More closely aligned with Asus's own code
     - Revert back to 10 years duration, as the Safari limitation only
       targets certificates signed by one of the built-in CAs
     - Remove CommonName entries as modern browsers no longer use that
       field - only hardcode router's IP address as the sole CN.
     - Ensure that hostname and computer_name are not the same to avoid
       duplicate SANs

--- release/src/router/httpd/gencert.sh
+++ release/src/router/httpd/gencert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
-PID=$$
 SECS=1262278080
+
+cd /etc
 
 if [ -f /usr/sbin/openssl11 ]
 then
@@ -10,12 +10,6 @@ else
 	OPENSSL=/usr/sbin/openssl
 fi
 
-if [ $(nvram get ntp_ready) = "1" ]
-then
-        DAYS=720
-else
-        DAYS=3653
-fi
 
 WAITTIMER=0
 while [ -f "/var/run/gencert.pid" -a $WAITTIMER -lt 14 ]
@@ -25,92 +19,72 @@ do
 done
 touch /var/run/gencert.pid
 
-cd /etc
-KEYNAME="key.pem"
-CERTNAME="cert.pem"
+OPENSSL_CONF="/etc/openssl.config"
 
-OPENSSLCNF="/etc/openssl.config.$PID"
-
-cp -L /etc/ssl/openssl.cnf $OPENSSLCNF
+cp -L /etc/ssl/openssl.cnf $OPENSSL_CONF
 
 LANCN=$(nvram get https_crt_cn)
 LANIP=$(nvram get lan_ipaddr)
 
-if [ "$LANCN" != "" ]
-then
-	I=0
-	for CN in $LANCN; do
-		echo "$I.commonName=CN" >> $OPENSSLCNF
-		echo "$I.commonName_value=$CN" >> $OPENSSLCNF
-		echo "$I.organizationName=O" >> $OPENSSLCNF
-		echo "$I.organizationName_value=$(uname -o)" >> $OPENSSLCNF
-		echo "$I.emailAddress=E" >> $OPENSSLCNF
-		echo "$I.emailAddress_value=root@localhost" >> $OPENSSLCNF
-		I=$(($I + 1))
-	done
-else
-	echo "0.commonName=CN" >> $OPENSSLCNF
-	echo "0.commonName_value=$LANIP" >> $OPENSSLCNF
-	echo "0.organizationName=O" >> $OPENSSLCNF
-	echo "0.organizationName_value=$(uname -o)" >> $OPENSSLCNF
-	echo "0.emailAddress=E" >> $OPENSSLCNF
-	echo "0.emailAddress_value=root@localhost" >> $OPENSSLCNF
-fi
+echo "0.commonName=CN" >> $OPENSSL_CONF
+echo "0.commonName_value=$LANIP" >> $OPENSSL_CONF
+echo "0.organizationName=O" >> $OPENSSL_CONF
+echo "0.organizationName_value=$(uname -o)" >> $OPENSSL_CONF
+echo "0.emailAddress=E" >> $OPENSSL_CONF
+echo "0.emailAddress_value=admin@router.asus.com" >> $OPENSSL_CONF
 
 # Required extension
-sed -i "/\[ v3_ca \]/aextendedKeyUsage = serverAuth" $OPENSSLCNF
+sed -i "/\[ v3_ca \]/aextendedKeyUsage = serverAuth" $OPENSSL_CONF
 
-I=0
 # Start of SAN extensions
-sed -i "/\[ CA_default \]/acopy_extensions = copy" $OPENSSLCNF
-sed -i "/\[ v3_ca \]/asubjectAltName = @alt_names" $OPENSSLCNF
-sed -i "/\[ v3_req \]/asubjectAltName = @alt_names" $OPENSSLCNF
-echo "[alt_names]" >> $OPENSSLCNF
+sed -i "/\[ CA_default \]/acopy_extensions = copy" $OPENSSL_CONF
+sed -i "/\[ v3_req \]/asubjectAltName = @alt_names" $OPENSSL_CONF
+echo "[alt_names]" >> $OPENSSL_CONF
 
+I=1
 # IP
-echo "IP.0 = $LANIP" >> $OPENSSLCNF
-echo "DNS.$I = $LANIP" >> $OPENSSLCNF # For broken clients like IE
+echo "IP.1 = $LANIP" >> $OPENSSL_CONF
+echo "DNS.1 = $LANIP" >> $OPENSSL_CONF # For broken clients like IE
 I=$(($I + 1))
 
 # DUT
-echo "DNS.$I = router.asus.com" >> $OPENSSLCNF
+echo "DNS.$I = router.asus.com" >> $OPENSSL_CONF
 I=$(($I + 1))
 
-# User-defined CN (if we have any)
+# User-defined SANs (if we have any)
 if [ "$LANCN" != "" ]
 then
 	for CN in $LANCN; do
-		echo "DNS.$I = $CN" >> $OPENSSLCNF
+		echo "DNS.$I = $CN" >> $OPENSSL_CONF
 		I=$(($I + 1))
 	done
 fi
 
 # hostnames
-
 LANDOMAIN=$(nvram get lan_domain)
 COMPUTERNAME=$(nvram get computer_name)
 LANHOSTNAME=$(nvram get lan_hostname)
 
 if [ "$COMPUTERNAME" != "" ]
 then
-	echo "DNS.$I = $COMPUTERNAME" >> $OPENSSLCNF
+	echo "DNS.$I = $COMPUTERNAME" >> $OPENSSL_CONF
 	I=$(($I + 1))
 
 	if [ "$LANDOMAIN" != "" ]
 	then
-		echo "DNS.$I = $COMPUTERNAME.$LANDOMAIN" >> $OPENSSLCNF
+		echo "DNS.$I = $COMPUTERNAME.$LANDOMAIN" >> $OPENSSL_CONF
 		I=$(($I + 1))
 	fi
 fi
 
-if [ "$LANHOSTNAME" != "" ]
+if [ "$LANHOSTNAME" != "" -a "$COMPUTERNAME" != "$LANHOSTNAME" ]
 then
-	echo "DNS.$I = $LANHOSTNAME" >> $OPENSSLCNF
+	echo "DNS.$I = $LANHOSTNAME" >> $OPENSSL_CONF
 	I=$(($I + 1))
 
 	if [ "$LANDOMAIN" != "" ]
 	then
-		echo "DNS.$I = $LANHOSTNAME.$LANDOMAIN" >> $OPENSSLCNF
+		echo "DNS.$I = $LANHOSTNAME.$LANDOMAIN" >> $OPENSSL_CONF
 		I=$(($I + 1))
 	fi
 fi
@@ -125,27 +99,26 @@ if [ "$(nvram get ddns_enable_x)" == "1" -a "$DDNSSERVER" != "WWW.DNSOMATIC.COM"
 then
 	if [ "$DDNSSERVER" == "WWW.NAMECHEAP.COM" -a "$DDNSUSER" != "" ]
 	then
-		echo "DNS.$I = $DDNSHOSTNAME.$DDNSUSER" >> $OPENSSLCNF
+		echo "DNS.$I = $DDNSHOSTNAME.$DDNSUSER" >> $OPENSSL_CONF
 		I=$(($I + 1))
 	else
-		echo "DNS.$I = $DDNSHOSTNAME" >> $OPENSSLCNF
+		echo "DNS.$I = $DDNSHOSTNAME" >> $OPENSSL_CONF
 		I=$(($I + 1))
 	fi
 fi
 
+# create the key and certificate request
+OPENSSL_CONF="/etc/openssl.config" $OPENSSL req -new -out /tmp/cert.csr -keyout /tmp/privkey.pem -newkey rsa:2048 -passout pass:password
 
-# create the key
-$OPENSSL genpkey -out $KEYNAME.$PID -algorithm rsa -pkeyopt rsa_keygen_bits:2048
-# create certificate request and sign it
-$OPENSSL req -new -x509 -key $KEYNAME.$PID -sha256 -out $CERTNAME.$PID -days $DAYS -config $OPENSSLCNF -set_serial $1
+# import the self-certificate
+OPENSSL_CONF="/etc/openssl.config" $OPENSSL rsa -in /tmp/privkey.pem -out key.pem -passin pass:password
+OPENSSL_CONF="/etc/openssl.config" RANDFILE=/dev/urandom $OPENSSL req -x509 -new -nodes -in /tmp/cert.csr -key key.pem -days 3653 -sha256 -out cert.pem
 
 # server.pem for WebDav SSL
-cat $KEYNAME.$PID $CERTNAME.$PID > server.pem
+cat key.pem cert.pem > server.pem
 
-mv $KEYNAME.$PID $KEYNAME
-mv $CERTNAME.$PID $CERTNAME
+cp cert.pem cert.crt
 
-chmod 640 $KEYNAME
-chmod 640 $CERTNAME
+chmod 640 key.pem
 
-rm -f /tmp/cert.csr $OPENSSLCNF /var/run/gencert.pid
+rm -f /tmp/cert.csr /tmp/privkey.pem $OPENSSL_CONF /var/run/gencert.pid

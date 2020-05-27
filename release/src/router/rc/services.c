5dcaa9f3a20e8d1b29145038d950f53575baac76
Eric Sauvageau <merlin@asuswrt-merlin.net>
2020-05-26 23:47:48 -0400

    rc: copy stored cert.crt when starting httpds
    Used for cert export function on the webui.

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -4972,6 +4972,7 @@ start_httpd(void)
                 if(f_exists(UPLOAD_CERT) && f_exists(UPLOAD_KEY)) {
                         eval("cp", UPLOAD_CERT, HTTPD_CERT);
                         eval("cp", UPLOAD_KEY, HTTPD_KEY);
+                        eval("cp", UPLOAD_CERT, "/etc/cert.crt");
 			system(tmp);
 		}
 	}
@@ -4979,6 +4980,7 @@ start_httpd(void)
 	if(f_exists(UPLOAD_CERT) && f_exists(UPLOAD_KEY)){
 		eval("cp", UPLOAD_CERT, HTTPD_CERT);
 		eval("cp", UPLOAD_KEY, HTTPD_KEY);
+		eval("cp", UPLOAD_CERT, "/etc/cert.crt");
 		system(tmp);
 	}
 #endif

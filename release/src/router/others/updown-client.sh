56ac6dd3a9cbf8456f3fad687c518abddf6897bf
Eric Sauvageau <merlin@asuswrt-merlin.net>
2020-07-25 00:48:44 -0400

    libovpn: also reapply DNS and QOS firewall rules when re-applying firewall rules
    This will fix exclusive DNS mode which would stop working after a
    firewall restart.

--- release/src/router/others/updown-client.sh
+++ release/src/router/others/updown-client.sh
@@ -130,12 +130,14 @@ then
 		echo /usr/sbin/iptables -t nat -I PREROUTING -p udp -m udp --dport 53 -j DNSVPN$instance >> $dnsscript
 		echo /usr/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 53 -j DNSVPN$instance >> $dnsscript
 	fi
+	chmod a+rx $dnsscript
 
 # QoS
 	if [ "$(nvram get vpn_client$(echo $instance)_rgw)" -ge 1 ] && [ "$(nvram get qos_enable)" -eq 1 ] && [ "$(nvram get qos_type)" -eq 1 ]
 	then
 		echo "#!/bin/sh" >> $qosscript
 		echo /usr/sbin/iptables -t mangle -A POSTROUTING -o br0 -m mark --mark 0x40000000/0xc0000000 -j MARK --set-xmark 0x80000000/0xC0000000 >> $qosscript
+		chmod a+rx $qosscript
 		/bin/sh $qosscript
 	fi
 fi

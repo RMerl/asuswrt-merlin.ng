9438fcf248b16e6438857a0a98bfc41145e51425
Eric Sauvageau <merlin@asuswrt-merlin.net>
2020-12-31 14:51:26 -0500

    webui: re-enable Speedtest webui on RT-AX56U and RT-Ax58U

--- release/src/router/www/state.js
+++ release/src/router/www/state.js
@@ -423,6 +423,8 @@ var wl_info = {
 
 function isSupport(_ptn){
 	var ui_support = [<% get_ui_support(); %>][0];
+	if (based_modelid == "RT-AX56U" || based_modelid == "RT-AX58U") // Kludge
+		ui_support["ookla"] = 1;
 	return (ui_support[_ptn]) ? ui_support[_ptn] : 0;
 }
 

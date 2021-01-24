43f641138d30fa5f5a0686b0970922b96f0d6f0e
Rokis <ghost1988102@gmail.com>
2021-01-24 22:26:15 +0800

    www: translate Game Boost for all devices.
    Signed-off-by: Rokis <ghost1988102@gmail.com>
    (cherry picked from commit a77b94c54de8800c976edee65f154c244dfa364c)

--- release/src/router/www/require/menuTrees/menuTree_bwdpi_traffic_analyzer.js
+++ release/src/router/www/require/menuTrees/menuTree_bwdpi_traffic_analyzer.js
@@ -445,13 +445,13 @@ define(function(){
 					retArray.push("menu_OpenNAT");
 				}
 
-				if(!rog_support && !tuf_support){
+				/*if(!rog_support && !tuf_support){
 					for(i=0; i<menuTree.list.length; i++){
 						if(menuTree.list[i].menuName == '<#Game_Boost#>'){
 							menuTree.list[i].menuName = 'Game';
 						}
 					}
-				}
+				}*/
 
 				if(!rrsut_support)
 					retArray.push("cloud_router_sync.asp");

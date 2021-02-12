39d4ebae135aede0f77a115fc01fcefd5ca9a648
Eric Sauvageau <merlin@asuswrt-merlin.net>
2021-02-12 17:37:11 -0500

    httpd: updated filename of json DB for caching

--- release/src/router/httpd/web.c
+++ release/src/router/httpd/web.c
@@ -19693,7 +19693,7 @@ struct mime_handler mime_handlers[] = {
 	{ "ureip.asp", "text/html", no_cache_IE7, do_html_post_and_get, do_ej, NULL },
 	{ "remote.asp", "text/html", no_cache_IE7, do_html_post_and_get, do_ej, NULL },
 	{ "js/jquery.js", "text/javascript", cache_object, NULL, do_file, NULL },
-	{ "js/ouiDB.js", "text/javascript", cache_object, NULL, do_file, NULL },
+	{ "js/ouiDB.json", "text/javascript", cache_object, NULL, do_file, NULL },
 	{ "js/chart.min.js", "text/javascript", cache_object, NULL, do_file, NULL },
 	{ "require/require.min.js", "text/javascript", cache_object, NULL, do_file, NULL },
 	{ "calendar/jquery-ui.js", "text/javascript", cache_object, NULL, do_file, NULL },

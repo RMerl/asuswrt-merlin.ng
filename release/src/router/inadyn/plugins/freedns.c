084fd02cbc9d2540c406e194213712a860ef7502
Eric Sauvageau <merlin@asuswrt-merlin.net>
2021-07-16 00:20:47 -0400

    inadyn: handle Freedns authentication errors as such; improve error logging
    Authentication errors when fetching keys were reported as generic
    errors, which means they would not properly terminate as a fatal error.
    
    Also, log errors with LOG_ERR instead of LOG_DEBUG or LOG_INFO,
    so they can be more easily seen by the user.

--- release/src/router/inadyn/plugins/freedns.c
+++ release/src/router/inadyn/plugins/freedns.c
@@ -88,7 +88,7 @@ static char *fetch_keys(ddns_t *ctx, ddns_info_t *info)
 	trans.max_rsp_len = ctx->work_buflen - 1;	/* Save place for a \0 at the end */
 
 	rc = http_transaction(&client, &trans);
-	logit(LOG_DEBUG, "=> %s", trans.rsp_body);
+	logit((strstr(trans.rsp_body, "ERROR:") ? LOG_ERR : LOG_DEBUG), "=> %s", trans.rsp_body);
 	http_exit(&client);
 	http_destruct(&client, 1);
 
@@ -110,8 +110,12 @@ static int setup(ddns_t *ctx, ddns_info_t *info, ddns_alias_t *alias)
 
 	tmp = buf = fetch_keys(ctx, info);
 	if (!buf) {
-		logit(LOG_INFO, "Cannot find you FreeDNS account API keys");
+		logit(LOG_ERR, "Cannot find you FreeDNS account API keys");
 		return RC_ERROR;
+	}
+
+	if (strstr(buf, "Could not authenticate")) {
+		return RC_DDNS_RSP_AUTH_FAIL;
 	}
 
 	for (line = strsep(&tmp, "\n"); line; line = strsep(&tmp, "\n")) {

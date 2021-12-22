2d97109f6e18308dda9acc4a0bbb71dd251990d1
Eric Sauvageau <merlin@asuswrt-merlin.net>
2021-12-22 13:03:13 -0500

    shared: Replace the source code of strlcpy() and strlcat() by BSD verison (patch from Asus upstream)
    The original strlcpy() and strlcat() is Apple version and it used memcpy().
    The source could not be overlap due to the limitation of memcpy().
    Replace these functions by FreeBSD version. It's the original version of these 2 functions.

--- release/src/router/shared/misc.c
+++ release/src/router/shared/misc.c
@@ -125,6 +125,7 @@ extern char *get_line_from_buffer(const char *buf, char *line, const int line_si
 
 #ifdef HND_ROUTER
 // defined (__GLIBC__) && !defined(__UCLIBC__)
+#if 0
 size_t strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t srclen, len;
@@ -152,6 +153,73 @@ size_t strlcat(char *dst, const char *src, size_t size)
 
 	return dstlen + strlcpy(null, src, size - dstlen);
 }
+#else /*Replace strlcpy and strlcat by the original version implemented by BSD*/
+/* Implementation of strlcpy() for platforms that don't already have it. */
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+size_t
+strlcpy(char *dst, const char *src, size_t siz)
+{
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	if(!dst || !src)
+		return 0;
+	/* Copy as many bytes as will fit */
+	if (n != 0) {
+		while (--n != 0) {
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
+       }
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (siz != 0)
+			*d = '\0';		/* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+	return(s - src - 1);	/* count does not include NUL */
+}
+
+/*
+ * Appends src to string dst of size siz (unlike strncat, siz is the
+ * full size of dst, not space left).  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
+ * Returns strlen(src) + MIN(siz, strlen(initial dst)).
+ * If retval >= siz, truncation occurred.
+ */
+size_t
+strlcat(char *dst, const char *src, size_t siz)
+{
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	size_t dlen;
+	if(!dst || !src)
+		return 0;
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+	return(dlen + (s - src));	/* count does not include NUL */
+}
+
+#endif
 #endif
 
 in_addr_t inet_addr_(const char *addr)

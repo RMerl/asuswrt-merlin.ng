RMerl/asuswrt-merlin.ng

commit 21c30e26727ece39ea88eb08172988c84d1ef48d

httpd: skip incomplete ARP cache entries while parsing guest clients

release/src/router/httpd/sysdeps/web-broadcom-am.c
@@ -681,10 +681,11 @@ sta_list:
 					arplistptr = strdup(arplist);
 					line = strtok(arplistptr, "\n");
 					while (line) {
-						if ( (sscanf(line,"%15s %*s %*s %17s",ipentry,macentry) == 2) &&
-						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[ii], ea))) ) {
-							found = 1;
-							break;
+						if ( (sscanf(line,"%15s %*s %x %17s", ipentry, &flagentry, macentry) == 3) &&
+						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea))) &&
+						     (flagentry != 0) ) {
+						         found = 1;
+						         break;
 						} else
 							line  = strtok(NULL, "\n");
 					}

commit 8b5cb095b15935a6dc2803cd90c415bd673b8bab

httpd: fix wireless client log issue with guests (issue introduced with 21c30e2672)

release/src/router/httpd/sysdeps/web-broadcom-am.c
@@ -682,7 +682,7 @@ sta_list:
 					line = strtok(arplistptr, "\n");
 					while (line) {
 						if ( (sscanf(line,"%15s %*s %x %17s", ipentry, &flagentry, macentry) == 3) &&
-						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea))) &&
+						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[ii], ea))) &&
 						     (flagentry != 0) ) {
 						         found = 1;
 						         break;
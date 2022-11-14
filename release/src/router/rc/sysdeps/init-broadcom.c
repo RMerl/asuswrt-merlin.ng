103042a5ea13ea5ca28205d64da632f8a4161853
Eric Sauvageau <merlin@asuswrt-merlin.net>
2022-11-13 22:25:16 -0500

    rc: leverage already existing is_wg_enabled() function in hnd_nat_ac_init()

--- release/src/router/rc/sysdeps/init-broadcom.c
+++ release/src/router/rc/sysdeps/init-broadcom.c
@@ -11333,23 +11333,7 @@ void hnd_nat_ac_init(int bootup)
 
 	// WG: keep FC disabled if WG server or clients  are enabled
 #ifdef RTCONFIG_WIREGUARD
-	for (unit = 1; unit <= WG_CLIENT_MAX; unit++ ) {
-		sprintf(buffer, "wgc%d_enable", unit);
-		if (nvram_get_int(buffer)) {
-			wg_enabled = 1;
-			break;
-		}
-	}
-
-	if (!wg_enabled) {
-		for (unit = 1; unit <= WG_SERVER_MAX; unit++ ) {
-			sprintf(buffer, "wgs%d_enable", unit);
-			if (nvram_get_int(buffer)) {
-				wg_enabled = 1;
-				break;
-			}
-		}
-	}
+	wg_enabled = is_wg_enabled();
 #endif
 
 	// Set fc_disable based on QOS type and WG states

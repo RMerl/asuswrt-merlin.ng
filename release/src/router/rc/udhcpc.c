827ad6eacf2f62bd8f7e7d9e6faed9edaeed6230
Eric Sauvageau <merlin@asuswrt-merlin.net>
2023-03-30 01:55:55 -0400

    rc: only refresh DDNS on bound6()/deconfig6() events if IPv6 DDNS updates are enabled

--- release/src/router/rc/udhcpc.c
+++ release/src/router/rc/udhcpc.c
@@ -1330,7 +1330,7 @@ deconfig6(char *wan_ifname, const int mode)
 #ifdef RTCONFIG_INADYN
 	if(mode == 1)
 	{
-		if (nvram_get_int("ddns_enable_x"))
+		if (nvram_get_int("ddns_enable_x") && nvram_get_int("ddns_ipv6_update"))
 			notify_rc("restart_ddns");
 	}
 #endif
@@ -1995,7 +1995,7 @@ skip:
 	}
 
 #ifdef RTCONFIG_INADYN
-	if (nvram_get_int("ddns_enable_x"))
+	if (nvram_get_int("ddns_enable_x") && nvram_get_int("ddns_ipv6_update"))
 		notify_rc("restart_ddns");
 #endif
 	return 0;

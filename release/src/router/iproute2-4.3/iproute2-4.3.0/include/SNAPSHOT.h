13cfbdf52646534a8d47594ad46b1a594051e727
Eric Sauvageau <merlin@asuswrt-merlin.net>
2023-06-07 01:03:31 -0400

    iproute2: remove iproute2-4.3, and move 5.11.0 up one level, inside router/
    This is more uniform with Asus now having different versions themselves, except
    we still use 5.11.0 for all our models, while Asus will use 4.3, 4.19 or 5.7
    depending on the platform.

--- release/src/router/iproute2-4.3/iproute2-4.3.0/include/SNAPSHOT.h
+++
@@ -1,1 +0,0 @@
-static const char SNAPSHOT[] = "151103";
--- release/src/router/iproute2-4.3/iproute2-4.3.0/tc/tc_cbq.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TC_CBQ_H_
-#define _TC_CBQ_H_ 1
-
-unsigned tc_cbq_calc_maxidle(unsigned bndw, unsigned rate, unsigned avpkt,
-			     int ewma_log, unsigned maxburst);
-unsigned tc_cbq_calc_offtime(unsigned bndw, unsigned rate, unsigned avpkt,
-			     int ewma_log, unsigned minburst);
-
-#endif

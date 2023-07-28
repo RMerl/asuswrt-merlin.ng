d08c709833898d398602c98c460c76ecaf034339
Eric Sauvageau <merlin@asuswrt-merlin.net>
2023-07-28 12:54:52 -0400

    rc: set ddns WAN unit variable on successful custom DDNS update
    Prevents the watchdog from re-running the update, thinking it was
    never run on the current WAN unit.

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -20610,6 +20610,7 @@ ddns_custom_updated_main(int argc, char *argv[])
 		nvram_set("ddns_server_x_old", nvram_safe_get("ddns_server_x"));
 		nvram_set("ddns_hostname_old", nvram_safe_get("ddns_hostname_x"));
 		logmessage("ddns", "Completed custom ddns update");
+		nvram_set_int("ddns_last_wan_unit", wan_primary_ifunit());
 	} else {
 		nvram_set("ddns_return_code", "unknown_error");
 		nvram_set("ddns_return_code_chk", "unknown_error");

85aa11716d0ae8a89c058a754be72f553ee2b812
Eric Sauvageau <merlin@asuswrt-merlin.net>
2023-08-29 18:04:57 -0400

    rc: trigger a DDNS retry attempt if WAN IP was not set yet following a down/up event (re-apply)

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -4585,6 +4585,7 @@ start_ddns(char *caller)
 
 	if (!wan_ip || (inet_addr_(wan_ip) == INADDR_ANY) || (nvram_get_int("link_internet") != 2)) {
 		logmessage("ddns", "WAN(%d) IP is empty.(%d)", unit, ddns_check_retry);
+		nvram_unset("ddns_updated");
 		return -1;
 	}
 

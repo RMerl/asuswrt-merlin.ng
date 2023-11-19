95151c4d345c49ffa156dde03e7fdc7bf26c8e3a
Eric Sauvageau <merlin@asuswrt-merlin.net>
2023-11-19 01:46:56 -0500

    openvpn: update to 2.6.8

--- release/src/router/openvpn/include/openvpn-plugin.h
+++ release/src/router/openvpn/include/openvpn-plugin.h
@@ -53,7 +53,7 @@ extern "C" {
  */
 #define OPENVPN_VERSION_MAJOR 2
 #define OPENVPN_VERSION_MINOR 6
-#define OPENVPN_VERSION_PATCH ".7"
+#define OPENVPN_VERSION_PATCH ".8"
 
 /*
  * Plug-in types.  These types correspond to the set of script callbacks

758995d4956dcb0d096c9f829c16bbac613d26bf
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-05-02 01:19:37 -0400

    webui: fix JS error on Wifi 6e models when enabling/disabling DDNS

--- release/src/router/www/sysdep/GT-AXE11000/www/general.js
+++ release/src/router/www/sysdep/GT-AXE11000/www/general.js
@@ -349,7 +349,6 @@ function change_common_radio(o, s, v, r){
 
 			change_ddns_setting(document.form.ddns_server_x.value);
 			inputCtrl(document.form.ddns_refresh_x, 1);
-			showhide("ddns_ipcheck_tr", 1);
 		}else{
 			if(document.form.ddns_server_x.value == "WWW.ASUS.COM"){
 				document.form.DDNSName.parentNode.parentNode.parentNode.style.display = "none";
@@ -369,7 +368,6 @@ function change_common_radio(o, s, v, r){
 			showhide("check_ddns_field", 0);
 			inputCtrl(document.form.ddns_regular_period, 0);
 			inputCtrl(document.form.ddns_refresh_x, 0);
-			showhide("ddns_ipcheck_tr", 0);
 
 			document.getElementById("ddns_status_tr").style.display = "none";
 			document.getElementById("ddns_result_tr").style.display = "none";

310dee2a45cb4b79a62b7b872550f3404c298829
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-05-16 22:25:19 -0400

    rc: implement RGB LED restart on GT-BE* models when re-enabling LEDs

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -22934,8 +22934,8 @@ void setup_leds()
 		start_ledg();
 		start_antled();
 #endif
-#if defined(GTAXE16000) || defined(GTAX11000_PRO)
-		notify_rc("restart_ledg");
+#if defined(GTAX11000_PRO) || defined(GTAXE16000) || defined(GTBE98) || defined(GTBE98_PRO) || defined(GTBE96)
+		notify_rc_and_wait_2min("restart_ledg");
 #endif
 #endif // RT-AX86U_PRO
 	}

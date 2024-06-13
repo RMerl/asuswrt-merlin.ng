f378f6d1a36360203d92eb98570cd5c9dc8979f3
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-06-12 23:19:56 -0400

    httpd: retrieve lease hostname by IP instead of by MAC for the wireless log page; other improvements to that code
    Some clients like the Google Pixel 8 seem to use different MACs for their DHCP lease
    versus wifi connectivity (might be related to MLO).  This commit should partly help, but
    there is no sure way to be able to associate the DHCP MAC with the assoc MAC if
    the client is currently moving around between MLO bands and the ARP doesn't have
    an entry associating the assoc MAC with its IP address.
    
    Removed the "<unknown>" labels used when a hostname/IP cannot be associated with
    a connected client so to look less "error-like" when one of these clients is
    encountered.

--- release/src/router/httpd/sysdeps/web-broadcom-am.c
+++ release/src/router/httpd/sysdeps/web-broadcom-am.c
@@ -351,7 +351,7 @@ ej_wl_unit_status_array(int eid, webs_t wp, int argc, char_t **argv, int unit)
 	char *ipv6list = NULL, *ipv6listptr;
 	char *line;
 	char hostnameentry[65];
-	char ipentry[1024], macentry[18];
+	char ipentry_arp[16], ipentry_dhcp[16], ipentry_ipv6[1024], macentry[18];
 	unsigned int flagentry;
 	int found, foundipv6 = 0, noclients = 0;
 	char rxrate[12], txrate[12];
@@ -541,7 +541,7 @@ sta_list:
 			arplistptr = strdup(arplist);
 			line = strtok(arplistptr, "\n");
 			while (line) {
-				if ( (sscanf(line,"%15s %*s %x %17s",ipentry,&flagentry,macentry) == 3) &&
+				if ( (sscanf(line,"%15s %*s %x %17s",ipentry_arp,&flagentry,macentry) == 3) &&
 				     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea))) &&
 				     (flagentry != 0) ) {
 					found = 1;
@@ -552,9 +552,9 @@ sta_list:
 			if (arplistptr)	free(arplistptr);
 
 			if (found || !leaselist)
-				ret += websWrite(wp, "\"%s\",", (found ? ipentry : "<unknown>"));
+				ret += websWrite(wp, "\"%s\",", (found ? ipentry_arp : ""));
 		} else {
-			ret += websWrite(wp, "\"<unknown>\",");
+			ret += websWrite(wp, "\"\",");
 		}
 
 		// Retrieve hostname from dnsmasq leases
@@ -562,8 +562,11 @@ sta_list:
 			leaselistptr = strdup(leaselist);
 			line = strtok(leaselistptr, "\n");
 			while (line) {
-				if ( (sscanf(line,"%*s %17s %15s %32s %*s", macentry, ipentry, tmp) == 3) &&
-				     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea))) ) {
+				if ( (sscanf(line,"%*s %17s %15s %32s %*s", macentry, ipentry_dhcp, tmp) == 3) &&
+				     ( (found > 0 && !strcmp(ipentry_arp, ipentry_dhcp)) ||
+				       (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea)))
+				     )
+				   ) {
 					found += 2;
 					break;
 				} else
@@ -573,23 +576,25 @@ sta_list:
 
 			if ((found) && (str_escape_quotes(hostnameentry, tmp, sizeof(hostnameentry)) == 0 ))
 				strlcpy(hostnameentry, tmp, sizeof(hostnameentry));
+			else
+				*hostnameentry = '\0';
 
 			switch (found) {
 			case 0:	// Not in arplist nor in leaselist
-				ret += websWrite(wp, "\"<unknown>\",\"<unknown>\",");
+				ret += websWrite(wp, "\"\",\"\",");
 				break;
 			case 1:	// Only in arplist (static IP)
-				ret += websWrite(wp, "\"<unknown>\",");
+				ret += websWrite(wp, "\"\",");
 				break;
 			case 2:	// Only in leaselist (dynamic IP that has not communicated with router for a while)
-				ret += websWrite(wp, "\"%s\", \"%s\",", ipentry, hostnameentry);
+				ret += websWrite(wp, "\"%s\", \"%s\",", ipentry_dhcp, hostnameentry);
 				break;
 			case 3:	// In both arplist and leaselist (dynamic IP)
 				ret += websWrite(wp, "\"%s\",", hostnameentry);
 				break;
 			}
 		} else {
-			ret += websWrite(wp, "\"<unknown>\",");
+			ret += websWrite(wp, "\"\",");
 		}
 
 #ifdef RTCONFIG_IPV6
@@ -597,9 +602,9 @@ sta_list:
 		if (ipv6list) {
 			ipv6listptr = ipv6list;
 			foundipv6 = 0;
-			while ((ipv6listptr < ipv6list+strlen(ipv6list)-2) && (sscanf(ipv6listptr,"%*s %17s %1023s", macentry, ipentry) == 2)) {
+			while ((ipv6listptr < ipv6list+strlen(ipv6list)-2) && (sscanf(ipv6listptr,"%*s %17s %1023s", macentry, ipentry_ipv6) == 2)) {
 				if (strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea)) == 0) {
-					ret += websWrite(wp, "\"%s\",", ipentry);
+					ret += websWrite(wp, "\"%s\",", ipentry_ipv6);
 					foundipv6 = 1;
 					break;
 				} else {
@@ -730,7 +735,7 @@ sta_list:
 					arplistptr = strdup(arplist);
 					line = strtok(arplistptr, "\n");
 					while (line) {
-						if ( (sscanf(line,"%15s %*s %x %17s", ipentry, &flagentry, macentry) == 3) &&
+						if ( (sscanf(line,"%15s %*s %x %17s", ipentry_arp, &flagentry, macentry) == 3) &&
 						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[ii], ea))) &&
 						     (flagentry != 0) ) {
 						         found = 1;
@@ -741,9 +746,9 @@ sta_list:
 					if (arplistptr) free(arplistptr);
 
 					if (found || !leaselist)
-						ret += websWrite(wp, "\"%s\",", (found ? ipentry : "<unknown>"));
+						ret += websWrite(wp, "\"%s\",", (found ? ipentry_arp : ""));
 				} else {
-					ret += websWrite(wp, "\"<unknown>\",");
+					ret += websWrite(wp, "\"\",");
 				}
 
 				// Retrieve hostname from dnsmasq leases
@@ -751,8 +756,11 @@ sta_list:
 					leaselistptr = strdup(leaselist);
 					line = strtok(leaselistptr, "\n");
 					while (line) {
-						if ( (sscanf(line,"%*s %17s %15s %32s %*s", macentry, ipentry, tmp) == 3) &&
-						     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[ii], ea))) ) {
+						if ( (sscanf(line,"%*s %17s %15s %32s %*s", macentry, ipentry_dhcp, tmp) == 3) &&
+						     ( (found > 0 && !strcmp(ipentry_arp, ipentry_dhcp)) ||
+						       (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea)))
+						     )
+						   ) {
 							found += 2;
 							break;
 						} else
@@ -762,24 +770,25 @@ sta_list:
 
 					if ((found) && (str_escape_quotes(hostnameentry, tmp,sizeof(hostnameentry)) == 0 ))
 						strlcpy(hostnameentry, tmp, sizeof(hostnameentry));
-
+					else
+						*hostnameentry = '\0';
 
 					switch (found) {
 					case 0:	// Not in arplist nor in leaselist
-						ret += websWrite(wp, "\"<not found>\",\"<not found>\",");
+						ret += websWrite(wp, "\"\",\"\",");
 						break;
 					case 1:	// Only in arplist (static IP)
-						ret += websWrite(wp, "\"<not found>\",");
+						ret += websWrite(wp, "\"\",");
 						break;
 					case 2:	// Only in leaselist (dynamic IP that has not communicated with router for a while)
-						ret += websWrite(wp, "\"%s\",\"%s\",", ipentry, hostnameentry);
+						ret += websWrite(wp, "\"%s\",\"%s\",", ipentry_dhcp, hostnameentry);
 						break;
 					case 3:	// In both arplist and leaselist (dynamic IP)
 						ret += websWrite(wp, "\"%s\",", hostnameentry);
 						break;
 					}
 				} else {
-					ret += websWrite(wp, "\"<unknown>\",");
+					ret += websWrite(wp, "\"\",");
 				}
 
 #ifdef RTCONFIG_IPV6
@@ -787,9 +796,9 @@ sta_list:
 				if (ipv6list) {
 					ipv6listptr = ipv6list;
 					foundipv6 = 0;
-					while ((ipv6listptr < ipv6list+strlen(ipv6list)-2) && (sscanf(ipv6listptr,"%*s %17s %1023s", macentry, ipentry) == 2)) {
+					while ((ipv6listptr < ipv6list+strlen(ipv6list)-2) && (sscanf(ipv6listptr,"%*s %17s %1023s", macentry, ipentry_ipv6) == 2)) {
 						if (strcasecmp(macentry, ether_etoa((void *)&auth->ea[ii], ea)) == 0) {
-							ret += websWrite(wp, "\"%s\",", ipentry);
+							ret += websWrite(wp, "\"%s\",", ipentry_ipv6);
 							foundipv6 = 1;
 							break;
 						} else {

b32934d2df082f1011f71c433018d8c57b3d28e0
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-07-04 00:33:32 -0400

    cfg_mnt: fix location of openssl.cnf; revert to upstream version of the gencert script

--- release/src/router/cfg_mnt/gencfgcert.sh
+++ release/src/router/cfg_mnt/gencfgcert.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 SECS=1262278080
 
-OPENSSL=/usr/sbin/openssl
-
 cd /etc
 mkdir cfg_mnt
 cd cfg_mnt
@@ -12,7 +10,7 @@ if [ "$NVCN" == "" ]; then
 	NVCN=`nvram get lan_ipaddr`
 fi
 
-cp -L /etc/ssl/openssl.cnf openssl.config
+cp -L /etc/openssl.cnf openssl.config
 
 I=0
 for CN in $NVCN; do
@@ -21,27 +19,13 @@ for CN in $NVCN; do
         I=$(($I + 1))
 done
 
-# Required extension
-sed -i "/\[ v3_ca \]/aextendedKeyUsage = serverAuth" openssl.config
-
-# Start of SAN extensions
-sed -i "/\[ CA_default \]/acopy_extensions = copy" openssl.config
-sed -i "/\[ v3_req \]/asubjectAltName = @alt_names" openssl.config
-echo "[alt_names]" >> openssl.config
-
-I=1
-for CN in $NVCN; do
-	echo "DNS.$I = $CN" >> openssl.config
-	I=$(($I + 1))
-done
-
 # create the key and certificate request
-OPENSSL_CONF=/etc/cfg_mnt/openssl.config $OPENSSL req -new -out /etc/cfg_mnt/cert.csr -keyout /etc/cfg_mnt/privkey.pem -newkey rsa:2048 -passout pass:password
+OPENSSL_CONF=/etc/cfg_mnt/openssl.config openssl req -new -out /etc/cfg_mnt/cert.csr -keyout /etc/cfg_mnt/privkey.pem -newkey rsa:2048 -passout pass:password
 # remove the passphrase from the key
-OPENSSL_CONF=/etc/cfg_mnt/openssl.cnf $OPENSSL rsa -in /etc/cfg_mnt/privkey.pem -out key.pem -passin pass:password
+OPENSSL_CONF=/etc/cfg_mnt/openssl.cnf openssl rsa -in /etc/cfg_mnt/privkey.pem -out key.pem -passin pass:password
 # convert the certificate request into a signed certificate
-OPENSSL_CONF=/etc/cfg_mnt/openssl.cnf RANDFILE=/dev/urandom $OPENSSL x509 -in /etc/cfg_mnt/cert.csr -out cert.pem -req -signkey key.pem -days 3653 -sha256
+OPENSSL_CONF=/etc/cfg_mnt/openssl.cnf RANDFILE=/dev/urandom openssl x509 -in /etc/cfg_mnt/cert.csr -out cert.pem -req -signkey key.pem -days 3653 -sha256
 
-$OPENSSL rsa -in key.pem -outform PEM -pubout -out pubkey.pem
+openssl rsa -in key.pem -outform PEM -pubout -out pubkey.pem
 
 rm -f /etc/cfg_mnt/cert.csr /etc/cfg_mnt/privkey.pem openssl.config

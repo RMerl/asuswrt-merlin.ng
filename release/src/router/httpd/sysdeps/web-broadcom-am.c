bff9ecb15a70e4b7ffcabefb4983b15412b97f92
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-09-11 01:29:53 -0400

    httpd: update wireless log backend code to handle main network now being subunit 1
    Primary network is now on wl0.1 instead of wl0.  Now use the same loop
    to handle both primary and guest networks STAs, also making the code
    simpler.

--- release/src/router/httpd/sysdeps/web-broadcom-am.c
+++ release/src/router/httpd/sysdeps/web-broadcom-am.c
@@ -293,7 +293,7 @@ wl_status_array(int eid, webs_t wp, int argc, char_t **argv, int unit)
 	char tmp[128], prefix[] = "wlXXXXXXXXXX_";
 	char *name;
 
-	snprintf(prefix, sizeof(prefix), "wl%d_", unit);
+	snprintf(prefix, sizeof(prefix), "wl%d.1_", unit);
 	name = nvram_safe_get(strcat_r(prefix, "ifname", tmp));
 
 	if ((ret = wl_ioctl(name, WLC_GET_BSSID, &bssid, ETHER_ADDR_LEN)) == 0) {
@@ -468,27 +468,11 @@ sta_list:
 // Open client array
 	ret += websWrite(wp, "wificlients%d = [", unit);
 
-#ifdef RTCONFIG_WIRELESSREPEATER
-	if ((nvram_get_int("sw_mode") == SW_MODE_REPEATER)
-		&& (nvram_get_int("wlc_band") == unit))
-	{
-		sprintf(name_vif, "wl%d.%d", unit, 1);
-		name = name_vif;
-	}
-#endif
-
 	/* buffers and length */
 	mac_list_size = sizeof(auth->count) + MAX_STA_COUNT * sizeof(struct ether_addr);
 	auth = malloc(mac_list_size);
 
 	if (!auth)
-		goto exit;
-
-	memset(auth, 0, mac_list_size);
-
-	/* query wl for authenticated sta list */
-	strcpy((char*)auth, "authe_sta_list");
-	if (wl_ioctl(name, WLC_GET_VAR, auth, mac_list_size))
 		goto exit;
 
 	/* Obtain mac + IP list */
@@ -525,181 +509,10 @@ sta_list:
 	}
 #endif
 
-	/* build authenticated sta list */
-	for (i = 0; i < auth->count; i ++) {
-		sta = wl_sta_info(name, &auth->ea[i]);
-		if (!sta) continue;
 
-		ret += websWrite(wp, "[\"%s\",", ether_etoa((void *)&auth->ea[i], ea));
+/*** Do all subunit client lists - subunit 1 = main interface ***/
 
-		found = 0;
-		if (arplist) {
-			arplistptr = strdup(arplist);
-			line = strtok(arplistptr, "\n");
-			while (line) {
-				if ( (sscanf(line,"%15s %*s %x %17s",ipentry_arp,&flagentry,macentry) == 3) &&
-				     (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea))) &&
-				     (flagentry != 0) ) {
-					found = 1;
-					break;
-				} else
-					line  = strtok(NULL, "\n");
-			}
-			if (arplistptr)	free(arplistptr);
-
-			if (found || !leaselist)
-				ret += websWrite(wp, "\"%s\",", (found ? ipentry_arp : ""));
-		} else {
-			ret += websWrite(wp, "\"\",");
-		}
-
-		// Retrieve hostname from dnsmasq leases
-		if (leaselist) {
-			leaselistptr = strdup(leaselist);
-			line = strtok(leaselistptr, "\n");
-			while (line) {
-				if ( (sscanf(line,"%*s %17s %15s %32s %*s", macentry, ipentry_dhcp, tmp) == 3) &&
-				     ( (found > 0 && !strcmp(ipentry_arp, ipentry_dhcp)) ||
-				       (!strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea)))
-				     )
-				   ) {
-					found += 2;
-					break;
-				} else
-					line = strtok(NULL, "\n");
-			}
-			if (leaselistptr) free(leaselistptr);
-
-			if (found) {
-				if (str_escape_quotes(hostnameentry, tmp, sizeof(hostnameentry)) == 0 )
-					strlcpy(hostnameentry, tmp, sizeof(hostnameentry));
-			} else {
-				*hostnameentry = '\0';
-			}
-
-			switch (found) {
-			case 0:	// Not in arplist nor in leaselist
-				ret += websWrite(wp, "\"\",\"\",");
-				break;
-			case 1:	// Only in arplist (static IP)
-				ret += websWrite(wp, "\"\",");
-				break;
-			case 2:	// Only in leaselist (dynamic IP that has not communicated with router for a while)
-				ret += websWrite(wp, "\"%s\", \"%s\",", ipentry_dhcp, hostnameentry);
-				break;
-			case 3:	// In both arplist and leaselist (dynamic IP)
-				ret += websWrite(wp, "\"%s\",", hostnameentry);
-				break;
-			}
-		} else {
-			ret += websWrite(wp, "\"\",");
-		}
-
-#ifdef RTCONFIG_IPV6
-// Retrieve IPv6
-		if (ipv6list) {
-			ipv6listptr = ipv6list;
-			foundipv6 = 0;
-			while ((ipv6listptr < ipv6list+strlen(ipv6list)-2) && (sscanf(ipv6listptr,"%*s %17s %1023s", macentry, ipentry_ipv6) == 2)) {
-				if (strcasecmp(macentry, ether_etoa((void *)&auth->ea[i], ea)) == 0) {
-					ret += websWrite(wp, "\"%s\",", ipentry_ipv6);
-					foundipv6 = 1;
-					break;
-				} else {
-					ipv6listptr = strstr(ipv6listptr,"\n")+1;
-				}
-			}
-		}
-#endif
-
-		if (foundipv6 == 0) {
-			ret += websWrite(wp, "\"\",");
-		}
-
-// RSSI
-		memcpy(&scb_val.ea, &auth->ea[i], ETHER_ADDR_LEN);
-		if (wl_ioctl(name, WLC_GET_RSSI, &scb_val, sizeof(scb_val_t)))
-			ret += websWrite(wp, "\"?\",");
-		else
-			ret += websWrite(wp, "\"%d\",", scb_val.val);
-
-		if (sta->flags & WL_STA_SCBSTATS)
-		{
-// Rate
-			if ((int)sta->rx_rate > 0)
-				sprintf(rxrate,"%d", sta->rx_rate / 1000);
-			else
-				strcpy(rxrate,"??");
-
-			if ((int)sta->tx_rate > 0)
-				sprintf(txrate,"%d", sta->tx_rate / 1000);
-			else
-				sprintf(txrate,"??");
-
-			ret += websWrite(wp, "\"%s\", \"%s\",", rxrate, txrate);
-
-// Connect time
-			hr = sta->in / 3600;
-			min = (sta->in % 3600) / 60;
-			sec = sta->in - hr * 3600 - min * 60;
-			ret += websWrite(wp, "\"%3d:%02d:%02d\",", hr, min, sec);
-
-// NSS
-#if (WL_STA_VER >= 5)
-			val =  wl_sta_info_nss(sta, unit);
-			if (val > 0)
-				ret += websWrite(wp, "\"%d\",", val);
-			else
-#endif
-				ret += websWrite(wp, "\"\",");
-
-// PHY
-			ret += websWrite(wp, "\"%s\",", phy_type_str_fmt[wl_sta_info_phy(sta, unit)]);
-
-// Bandwidth
-#if (WL_STA_VER >= 7)
-			if (sta->flags & WL_STA_SCBSTATS)
-				ret += websWrite(wp, "\"%s\",", wl_bw_str_fmt[wl_sta_info_bw(sta)]);
-			else
-#endif
-				ret += websWrite(wp, "\"\",");
-
-// Flags
-#ifdef RTCONFIG_BCMARM
-			ret += websWrite(wp, "\"%s%s%s",
-				(sta->flags & WL_STA_PS) ? "P" : "_",
-				((sta->ht_capabilities & (WL_STA_CAP_SHORT_GI_20 | WL_STA_CAP_SHORT_GI_40))) ? "S" : "_",
-				((sta->ht_capabilities & (WL_STA_CAP_TX_STBC | WL_STA_CAP_RX_STBC_MASK))
-#if (WL_STA_VER >= 7)
-				|| (sta->he_flags & (WL_STA_HE_TX_STBCCAP | WL_STA_HE_RX_STBCCAP))
-#endif
-				) ? "T" : "_");
-#ifdef RTCONFIG_MUMIMO
-			ret += websWrite(wp, "%s",
-				((sta->vht_flags & (WL_STA_MU_BEAMFORMER | WL_STA_MU_BEAMFORMEE))
-#if (WL_STA_VER >= 7)
-				 || (sta->he_flags & (WL_STA_HE_SU_BEAMFORMER | WL_STA_HE_MU_BEAMFORMER | WL_STA_HE_SU_MU_BEAMFORMEE))
-#endif
-				) ? "M" : "_");
-#endif
-#else
-			ret += websWrite(wp, "\"%s",
-				(sta->flags & WL_STA_PS) ? "P" : "_");
-#endif
-		}
-		ret += websWrite(wp, "%s%s\",",
-			(sta->flags & WL_STA_ASSOC) ? "A" : "_",
-			(sta->flags & WL_STA_AUTHO) ? "U" : "_");
-
-
-// SSID and VLAN (for Guest Networks - unused on main unit)
-		ret += websWrite(wp, "\"\",\"\"],");
-	}
-
-
-/*** Do Guest Networks ***/
-
-	for (i = 1; i < MAX_GUEST_SUBUNITS; i++) {
+	for (i = 1; i < MAX_NO_MSSID; i++) {
 #ifdef RTCONFIG_WIRELESSREPEATER
 		if ((nvram_get_int("sw_mode") == SW_MODE_REPEATER)
 			&& (unit == nvram_get_int("wlc_band")) && (i == 1))
@@ -710,10 +523,13 @@ sta_list:
 		{
 			sprintf(name_vif, "wl%d.%d", unit, i);
 
-			strlcpy(guestssid, nvram_pf_safe_get(prefix, "ssid"), sizeof(guestssid));
+// Not primary interface - retrieve ssid and VLAN
+			if (i != 1) {
+				strlcpy(guestssid, nvram_pf_safe_get(prefix, "ssid"), sizeof(guestssid));
 #ifdef RTCONFIG_MULTILAN_CFG
-			guestvlan = get_apg_vid_by_ifname(name_vif);
+				guestvlan = get_apg_vid_by_ifname(name_vif);
 #endif
+			}
 
 			memset(auth, 0, mac_list_size);
 
@@ -880,15 +696,19 @@ sta_list:
 					(sta->flags & WL_STA_ASSOC) ? "A" : "_",
 					(sta->flags & WL_STA_AUTHO) ? "U" : "_");
 
+// If not a Guest Network then don't push SSID and VLAN in client list
+				if (i == 1)
+			                ret += websWrite(wp, "\"\",\"\"],");
+				else {
 // SSID (for Guest Networks identification)
-				ret += websWrite(wp, "\"%s\",", guestssid);
-
+					ret += websWrite(wp, "\"%s\",", guestssid);
 // VLAN
 #ifdef RTCONFIG_MULTILAN_CFG
-				ret += websWrite(wp, "\"%d\"],", guestvlan);
+					ret += websWrite(wp, "\"%d\"],", guestvlan);
 #else
-				ret += webswrite(wp, "\"\"],");
+					ret += webswrite(wp, "\"\"],");
 #endif
+				}
 			}
 		}
 	}

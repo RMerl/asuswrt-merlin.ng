ac0acae5f4e811fa9243496885cf3b4ca29c221b
Eric Sauvageau <merlin@asuswrt-merlin.net>
2024-12-24 22:14:14 -0500

    rom: implement HTTP alternatives to gettunnelip.sh, and make it default method
    stun lookups through a VPN would sometime randomly fail.  These HTTP-based
    methods should be more reliable.

--- release/src/router/rom/apps_scripts/gettunnelip.sh
+++ release/src/router/rom/apps_scripts/gettunnelip.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 INSTANCE=$1
 PROTO=$2
+METHOD=$3
 
-servers="stun.l.google.com:19302 stun.stunprotocol.org"
+http_servers="api.ipify.org ident.me"
+stun_servers="stun.l.google.com:19302 stun.cloudflare.com:3478"
 
 if [ "$PROTO" == "wireguard" ] ; then
 	IFACE="wgc$INSTANCE"
@@ -11,11 +13,20 @@ else
 fi
 
 
-for server in $servers; do
-	result=$(/usr/sbin/ministun -t 5000 -c 1 -i $IFACE $server 2>/dev/null)
-	[ $? -eq 0 ] && break
-	result="unknown"
-done
+if [ "$METHOD" == "stun" ] ; then
+	for server in $stun_servers; do
+		result=$(/usr/sbin/ministun -t 5000 -c 1 -i $IFACE $server 2>/dev/null)
+		[ $? -eq 0 ] && break
+		result="unknown"
+	done
+else
+	for server in $http_servers; do
+		result=$(/usr/sbin/curl -4 -m 5 -sf --interface $IFACE $server 2>/dev/null)
+		[ $? -eq 0 ] && break
+		result="unknown"
+	done
+fi
+
 
 if [ "$PROTO" == "wireguard" ] ; then
 	nvram set "wgc${INSTANCE}_rip"=$result

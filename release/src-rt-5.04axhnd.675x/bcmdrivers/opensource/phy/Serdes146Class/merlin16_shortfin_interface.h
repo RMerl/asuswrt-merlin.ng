8bda787c9ce4019259f14da68b522884067e757d
Eric Sauvageau <merlin@asuswrt-merlin.net>
2025-06-19 12:15:18 -0400

    Merge 5.04ax SDK from 102_36957

---
+++ release/src-rt-5.04axhnd.675x/bcmdrivers/opensource/phy/Serdes146Class/merlin16_shortfin_interface.h
@@ -0,0 +1,67 @@
+/*
+   Copyright (c) 2015 Broadcom Corporation
+   All Rights Reserved
+
+    <:label-BRCM:2015:DUAL/GPL:standard
+
+    Unless you and Broadcom execute a separate written software license
+    agreement governing use of this software, this software is licensed
+    to you under the terms of the GNU General Public License version 2
+    (the "GPL"), available at http://www.broadcom.com/licenses/GPLv2.php,
+    with the following added to such license:
+
+       As a special exception, the copyright holders of this software give
+       you permission to link this software with independent modules, and
+       to copy and distribute the resulting executable under terms of your
+       choice, provided that you also meet, for each linked independent
+       module, the terms and conditions of the license of that module.
+       An independent module is a module which is not derived from this
+       software.  The special exception does not apply to any modifications
+       of the software.
+
+    Not withstanding the above, under no circumstances may you combine
+    this software in any way with any other Broadcom software provided
+    under a license other than the GPL, without Broadcom's express prior
+    written consent.
+
+:>
+*/
+/*! \mainpage
+ *
+ * \section intro_sec Introduction
+ *
+ * This documentation files are intended to guide a developer in using the
+ * Broadcom SerDes core within a larger ecosystem by providing specific API
+ * function calls which can be used to perform all necessary operations.
+ *
+ * The detailed description about the API directory structure, API overview,
+ * release procedure, and check out process can be found in the Serdes API User Guide
+ * (https://broadcom.box.com/v/merlin16_shortfin-common-16nm)
+ * \n \n
+ *
+ * \section API_sec Important API Files
+ *
+ * merlin16_shortfin_interface.h - provides the primary functionality.
+ *
+ * merlin16_shortfin_dependencies.h - defines the functions to be provided by the IP user
+ *
+ * \n \n
+ * Copyright: (c) 2019 Broadcom.                                                   *
+ * Broadcom Proprietary and Confidential. All rights reserved.                     *
+ */
+
+/** @file merlin16_shortfin_interface.h
+ * Main interface functions provided to IP User
+ */
+
+#ifndef MERLIN16_SHORTFIN_API_INTERFACE_H
+#define MERLIN16_SHORTFIN_API_INTERFACE_H
+
+#include "merlin16_shortfin_access.h"
+#include "merlin16_shortfin_config.h"
+#include "merlin16_shortfin_debug_functions.h"
+#include "merlin16_shortfin_diag.h"
+#include "merlin16_shortfin_prbs.h"
+#include "merlin16_shortfin_pwr_mgt.h"
+
+#endif

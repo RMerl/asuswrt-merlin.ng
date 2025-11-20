a6192a835c1bc44773a547f1cae2737114cb26ff
Eric Sauvageau <merlin@asuswrt-merlin.net>
2025-11-19 23:27:52 -0500

    shared: fix non-working jffs2_scripts setting on GT-BE19000AI

--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -5075,7 +5075,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "nfsd_exportlist", "", CKN_STR2048, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 #endif
 
-#if defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2) || defined(RTCONFIG_UBIFS)
+#if defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2) || defined(RTCONFIG_UBIFS) || defined(RTCONFIG_JFFS_PARTITION)
 	{ "jffs2_scripts", "0", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 #ifdef RTCONFIG_UBIFS
 	{ "ubifs_format", "0", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
